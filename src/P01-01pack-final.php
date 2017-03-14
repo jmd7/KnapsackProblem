bd0d4da2ddb3ead25912df9c7f0ea418cae4693b
Hu Bin <hubin.beijing@aliyun.com>
2017-03-14 20:35:25 +0800

    Add 2 of other less-effective solutions for 01pack.

--- src/P01-01pack-final.php
+++ src/P01-01pack-final.php
@@ -4,26 +4,29 @@ require_once "KnapsackItem.php";
 require_once "KnapsackPack.php";
 require_once "AbstractKnapsackSolution.php";
 
-class ZeroOnePack_Solution_01 extends AbstractKnapsackSolution {
+class ZeroOnePack_Solution_final extends AbstractKnapsackSolution {
     public static function fillPack(array $items, KnapsackPack $pack, bool $fitPackVolume = false) {
         $N = count($items);
         $V = $pack->getVolume();
         $loop_count = 0;
 
         if ($fitPackVolume) {
-            $f = array_fill(0, $N+1, array_fill(0, $V+1, null));
-            for ($i = 0; $i <= $N; $i++) $f[$i][0] = 0;
-        } else $f = array_fill(0, $N+1, array_fill(0, $V+1, 0));
-        $g = array_fill(0, $V+1, -1);
+            $f = array_fill(0, $V+1, null);
+            $f[0] = 0;
+        } else $f = array_fill(0, $V+1, 0);
+        $g = array_fill(0, count($f), -1);
 
-        for ($i = 1; $i <= $N; $i++) {
-            for ($v = $items[$i-1]->getCost(); $v <= $V; $v++) {
-                $left = is_null($f[$i-1][$v-$items[$i-1]->getCost()]) ? null : $f[$i-1][$v-$items[$i-1]->getCost()] + $items[$i-1]->getValue();
-                $right = $f[$i-1][$v];
+        for ($i = 0; $i < $N; $i++) {
+            $sum = 0;
+            for ($j = $i; $j < $N; $j++) $sum += $items[$j]->getCost();
+            $bound = self::kp_max($V-$sum, $items[$i]->getCost());
+            for ($v = $V; $v >= $bound; $v--) {
+                $left = is_null($f[$v-$items[$i]->getCost()]) ? null : $f[$v-$items[$i]->getCost()] + $items[$i]->getValue();
+                $right = $f[$v];
                 $left_item = $i;
                 $right_item = $g[$v];
                 
-                $f[$i][$v] = self::kp_max_tracing($left, $right, $g[$v], $left_item, $right_item);
+                $f[$v] = self::kp_max_tracing($left, $right, $g[$v], $left_item, $right_item);
                 $loop_count++;
             }
         }
@@ -32,12 +35,12 @@ class ZeroOnePack_Solution_01 extends AbstractKnapsackSolution {
 
         $res = array();
         $res["Loop count"] = $loop_count;
-        $res["Value of best solution"] = $f[$N][$V];
+        $res["Value of best solution"] = $f[$V];
         $res["Items of best solution"] = array();
         $V_real = $V;
-        while ($f[$N][$V_real] == $f[$N][$V_real -1]) $V_real--;
-        for ($i = $V_real; $i > 0 && $g[$i] >= 0; $i = $i - $items[$g[$i]-1]->getCost()) {
-            $res["Items of best solution"][] = $items[$g[$i]-1]->getName();
+        while ($f[$V_real] == $f[$V_real -1]) $V_real--;
+        for ($i = $V_real; $i > 0 && $g[$i] >= 0; $i = $i - $items[$g[$i]]->getCost()) {
+            $res["Items of best solution"][] = $items[$g[$i]]->getName();
             // echo $items[$g[$i]]->getName()."\n";
         }
         // $res["Items"] = $items;
@@ -59,7 +62,7 @@ $items[] = new KnapsackItem("甜瓜", 6, 6700, 1);
 
 $pack = new KnapsackPack("背包", 13);
 
-ZeroOnePack_Solution_01::run($items, $pack, false);
-ZeroOnePack_Solution_01::run($items, $pack, true);
+ZeroOnePack_Solution_final::run($items, $pack, false);
+ZeroOnePack_Solution_final::run($items, $pack, true);
 
 ?>
